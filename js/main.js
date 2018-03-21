cca1cd5247db6141261c816b05996f08fbe1f743
Fernando Espinosa Victoriano <fespinosa@fespinosa.nt.core>
2018-03-21 11:47:18 -0400

    add mvc arch

--- js/main.js
+++ js/main.js
@@ -1,30 +1,53 @@
-
-
+'strict mode'
 class CatsAreaView {
-
-    render() {
-
+    constructor(octopus) {
+        this.octopus = octopus;
+        this.numberOfClicks = 0;
     }
+    render() {
+        let catsArea = document.querySelector(".cats-area");
+        let div = document.createElement("div");
+        div.setAttribute("class", "cat");
+        let img = document.createElement("img");
+        img.setAttribute("src", octopus.currentCat.image);
+        img.setAttribute("alt", "Cat image");
+        let h3 = document.createElement("h3");
+        h3.innerText = octopus.currentCat.name;
+        let h2 = document.createElement("h2");
+        h2.innerText = `You've clicked ${octopus.currentCat.numberOfClicks} time(s)`;
+        div.appendChild(h3);
+        div.appendChild(h2);
+        div.appendChild(img);
+        div.addEventListener("click", () => {
+            octopus.increaseClickCount();
+        }); 
+        catsArea.innerHTML = '';
+        catsArea.appendChild(div);
+    }
+
 }
 
 class CatsListView {
-    constructor() {
+    constructor(octopus) {
+        this.octopus = octopus;
         this.nav = document.querySelector("nav");
-        this.ul = document.createElement("ul");
-       
+        
     }
 
     render() {
-        for (let index = 0; index < octopus.getCats().length; index++) {
-            const cat = this.cats[index];
-            this.li = document.createElement("li");
-            this.li.innerText = cat.name;
+        let ul = document.createElement("ul");
+        let cats = this.octopus.getCats();
+        for (let index = 0; index < cats.length; index++) {
+            const cat = cats[index];
+            let li = document.createElement("li");
+            li.innerText = cat.name;
 
-            this.li.addEventListener("click", () => {
-                this.displayOnCatsArea(cat);
+            li.addEventListener("click", () => {
+                octopus.displayOnCatsArea(cat);
+
             });
 
-            this.ul.appendChild(li);
+            ul.appendChild(li);
 
         }
         this.nav.appendChild(ul);
@@ -33,25 +56,48 @@ class CatsListView {
 
 class Octopus {
     constructor() {
-        this.cats = [
-            cat1 = new Cat("https://static.pexels.com/photos/104827/cat-pet-animal-domestic-104827.jpeg", "Meli"),
-            cat2 = new Cat("https://static.pexels.com/photos/45201/kitty-cat-kitten-pet-45201.jpeg", "Quico")
-        ];
+        this.currentCat = null;
+        this.catsListView = new CatsListView(this);
+        this.catsAreaView = new CatsAreaView(this);
+        this.catsListView.render();
 
     }
 
     getCats() {
-        return this.cats;
+        return model.getCats();
+    }
+    
+    displayOnCatsArea(cat) {
+        this.currentCat = cat;
+        this.catsAreaView.render();
     }
 
-    
+    increaseClickCount() {
+        this.currentCat.numberOfClicks += 1;
+        this.catsAreaView.render();
+    }
+
+   
 }
 
 class Cat {
     constructor(image, name) {
         this.image = image;
         this.name = name;
+        this.numberOfClicks = 0;
     }
 }
 
+var model = {
+    cats : [new Cat("https://static.pexels.com/photos/104827/cat-pet-animal-domestic-104827.jpeg", "Meli"),
+    new Cat("https://static.pexels.com/photos/45201/kitty-cat-kitten-pet-45201.jpeg", "Quico")],
+    getCats : function(){
+        return this.cats;
+    }
+
+}
+
+
 var octopus = new Octopus();
+
+
