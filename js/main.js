2d6a762cb4126e6128a16c3e6879b0a0d66a6eb3
Fernando Espinosa <fjevictoriano@gmail.com>
2018-03-24 10:32:11 -0400

    Refactor arch to improve performance

--- js/main.js
+++ js/main.js
@@ -2,78 +2,93 @@
 class CatsAreaView {
     constructor(octopus) {
         this.octopus = octopus;
-        this.numberOfClicks = 0;
     }
-    render() {
-        let catsArea = document.querySelector(".cats-area");
-        let div = document.createElement("div");
-        div.setAttribute("class", "cat");
-        let img = document.createElement("img");
-        img.setAttribute("src", octopus.currentCat.image);
-        img.setAttribute("alt", "Cat image");
-        let h3 = document.createElement("h3");
-        h3.innerText = octopus.currentCat.name;
-        let h2 = document.createElement("h2");
-        h2.innerText = `You've clicked ${octopus.currentCat.numberOfClicks} time(s)`;
-        div.appendChild(h3);
-        div.appendChild(h2);
-        div.appendChild(img);
-        div.addEventListener("click", () => {
+
+    init(){
+       this.catElem = document.querySelector("#cat");
+       this.catNameElem = document.querySelector("#cat-name");
+       this.catImageElem = document.querySelector("#cat-image");
+       this.clickCountElem = document.querySelector("#click-count");
+
+        this.catImageElem.addEventListener("click", () => {
             octopus.increaseClickCount();
         }); 
-        catsArea.innerHTML = '';
-        catsArea.appendChild(div);
+
+        this.render();
+
+    }
+
+    render() {
+        let currentCat = octopus.getCurrentCat();
+        this.catNameElem.textContent = currentCat.name;
+        this.catImageElem.src = currentCat.image;
+        this.clickCountElem.textContent = currentCat.numberOfClicks;
     }
 
 }
 
 class CatsListView {
     constructor(octopus) {
-        this.octopus = octopus;
-        this.nav = document.querySelector("nav");
-        
+        this.octopus = octopus;        
+    }
+
+    init(){
+        this.catListElem = document.querySelector("#cat-list");
+
+        this.render();
     }
 
     render() {
-        let ul = document.createElement("ul");
         let cats = this.octopus.getCats();
+
+        this.catListElem.innerHTML = "";
+
         for (let index = 0; index < cats.length; index++) {
             const cat = cats[index];
-            let li = document.createElement("li");
-            li.innerText = cat.name;
-
-            li.addEventListener("click", () => {
+            let liElem = document.createElement("li");
+            liElem.textContent = cat.name;
+            liElem.addEventListener("click", () => {
                 octopus.displayOnCatsArea(cat);
 
             });
-
-            ul.appendChild(li);
+            this.catListElem.appendChild(liElem);
 
         }
-        this.nav.appendChild(ul);
     }
 }
 
 class Octopus {
     constructor() {
-        this.currentCat = null;
         this.catsListView = new CatsListView(this);
         this.catsAreaView = new CatsAreaView(this);
-        this.catsListView.render();
+    }
 
+    init(){
+        model.currentCat = model.cats[0];
+
+        this.catsListView.init();
+        this.catsAreaView.init();
     }
 
     getCats() {
-        return model.getCats();
+        return model.cats;
+    }
+
+    setCurrentCat(cat){
+        model.currentCat = cat;
+    }
+
+    getCurrentCat(){
+        return model.currentCat;
     }
     
     displayOnCatsArea(cat) {
-        this.currentCat = cat;
+        model.currentCat = cat;
         this.catsAreaView.render();
     }
 
     increaseClickCount() {
-        this.currentCat.numberOfClicks += 1;
+        model.currentCat.numberOfClicks += 1;
         this.catsAreaView.render();
     }
 
@@ -89,15 +104,13 @@ class Cat {
 }
 
 var model = {
+    currentCat: null,
     cats : [new Cat("https://static.pexels.com/photos/104827/cat-pet-animal-domestic-104827.jpeg", "Meli"),
-    new Cat("https://static.pexels.com/photos/45201/kitty-cat-kitten-pet-45201.jpeg", "Quico")],
-    getCats : function(){
-        return this.cats;
-    }
-
+    new Cat("https://static.pexels.com/photos/45201/kitty-cat-kitten-pet-45201.jpeg", "Quico")]
 }
 
 
 var octopus = new Octopus();
+octopus.init();
 
 
